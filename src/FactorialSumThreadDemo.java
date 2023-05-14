6ffb6dd7b37be28e7143a8a1c72047e6ec6fa865
lrsm21427 <1686180008@qq.com>
2023-05-15 00:10:15 +0800

    Initial commit

--- src/FactorialSumThreadDemo.java
+++ src/FactorialSumThreadDemo.java
@@ -4,9 +4,10 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.math.BigInteger;
 import java.text.DecimalFormat;
-import java.util.concurrent.TimeUnit;
 import java.awt.Color;
+import java.util.Random;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -16,58 +17,67 @@ import javax.swing.SwingUtilities;
 
 public class FactorialSumThreadDemo extends JFrame {
     private static final long serialVersionUID = 1L;
-
+    private final Random rand = new Random();
     private JLabel sumLabel;
     private JLabel progressLabel;
     private JProgressBar progressBar;
     private JPanel progressPanel;
+    private JButton pauseButton;
 
     private int numTerms;
-    private BigInteger  sum;
+    private BigInteger sum;
     private double progress;
+    private boolean isPaused;
+
+    private FactorialSumThread thread;
 
     public FactorialSumThreadDemo() {
-        super("Factorial Sum Thread Demo");
-        setSize(600, 250);
+        super("阶乘和");
+        setSize(700, 250);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // Create the sum label
         sumLabel = new JLabel();
         sumLabel.setFont(new Font("SansSerif", Font.BOLD, 20));
         sumLabel.setHorizontalAlignment(JLabel.CENTER);
 
-        // Create the progress label
         progressLabel = new JLabel();
         progressLabel.setFont(new Font("SansSerif", Font.PLAIN, 16));
         progressLabel.setHorizontalAlignment(JLabel.CENTER);
 
-        // Create the progress bar
         progressBar = new JProgressBar(SwingConstants.HORIZONTAL, 0, 100);
         progressBar.setPreferredSize(new Dimension(400, 20));
 
-        // Create the progress panel
         progressPanel = new JPanel(new GridLayout(1, numTerms));
         progressPanel.setPreferredSize(new Dimension(400, 40));
 
-        // Add the labels and panel to the content pane
+        pauseButton = new JButton("暂停");
+        pauseButton.addActionListener(e -> {
+            isPaused = !isPaused;
+            pauseButton.setText(isPaused ? "开始" : "暂停");
+            if (isPaused) {
+                thread.pauseThread();
+            } else {
+                thread.resumeThread();
+            }
+        });
+
         getContentPane().add(sumLabel, BorderLayout.CENTER);
         getContentPane().add(progressLabel, BorderLayout.NORTH);
 
-
-        // Create and start the factorial sum thread
         numTerms = 30;
         sum = BigInteger.ZERO;
         progress = 0.0;
-        FactorialSumThread thread = new FactorialSumThread(numTerms,progressBar);
-        thread.start();
+        isPaused = false;
 
-        //Create a new panel to hold the progress bar and progress panel
+        thread = new FactorialSumThread(numTerms, progressBar);
+        thread.start();
+//        thread.pauseThread();
         JPanel progressPanelWrapper = new JPanel(new BorderLayout());
         progressPanelWrapper.add(progressBar, BorderLayout.NORTH);
         progressPanelWrapper.add(progressPanel, BorderLayout.CENTER);
         getContentPane().add(progressPanelWrapper, BorderLayout.SOUTH);
+        getContentPane().add(pauseButton, BorderLayout.WEST);
 
-        // Create and start the progress thread
         ProgressThread progressThread = new ProgressThread();
         progressThread.start();
     }
@@ -80,79 +90,79 @@ public class FactorialSumThreadDemo extends JFrame {
         private int numTerms;
         private BigInteger sum;
         private JProgressBar progressBar;
+        private boolean isThreadPaused;
 
         public FactorialSumThread(int numTerms, JProgressBar progressBar) {
             this.numTerms = numTerms;
             this.sum = BigInteger.ZERO;
             this.progressBar = progressBar;
+            this.isThreadPaused = false;
         }
 
         public void run() {
             for (int i = 1; i <= numTerms; i++) {
-                // 计算 i 的阶乘
+                synchronized (this) {
+                    while (isThreadPaused) {
+                        try {
+                            wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
                 BigInteger fact = BigInteger.ONE;
                 for (int j = 2; j <= i; j++) {
                     fact = fact.multiply(BigInteger.valueOf(j));
                 }
 
-                // 将阶乘加到总和中
                 sum = sum.add(fact);
 
-                // Update progress bar on event dispatch thread
                 progress = (double) i / numTerms;
 
                 progressBar.setForeground(Color.GRAY);
-                SwingUtilities.invokeLater(new Runnable() {
-                    public void run() {
-                        progressBar.setValue((int) ((progress-(1/30))*100));
-                    }
+                SwingUtilities.invokeLater(() -> {
+                    progressBar.setValue((int) ((progress - (1.0 / numTerms)) * 100));
                 });
-
-                // 输出当前的总和
                 DecimalFormat df = new DecimalFormat("#,###");
                 System.out.println("当前总和： " + df.format(sum));
-                System.out.println("到i的阶乘和： " + df.format((progress-(1/30))*100));
-                // 等待 0.5-1.5 秒的随机时间
-                try {
-//                    Thread.sleep((long) (500 + Math.random() * 1000));
-                    Thread.sleep((long) (1000));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-                // 更新进度
-                progress = (double) i / numTerms;
-
-                DecimalFormat dt = new DecimalFormat("#,###");
-                sumLabel.setText("1到"+ i +"的阶乘和为： " + df.format(sum));
-            }
-
-            // 输出最终总和
-//            DecimalFormat df = new DecimalFormat("#,###");
-//            System.out.println("总和： " + df.format(sum));
-            // 更新总和标签
-//            df = new DecimalFormat("#,###");
-//            sumLabel.setText("总和： " + df.format(sum));
-        }
-    }
-
-
-    private class ProgressThread extends Thread {
-        public void run() {
-            while (true) {
-                // Update the progress label and bar
-                DecimalFormat df = new DecimalFormat("0.00%");
-                progressLabel.setText("Progress: " + df.format(progress));
-                progressBar.setValue((int) (progress * 100));
-
-                // Sleep for 1 second
+                System.out.println("到i的阶乘和： " + df.format((progress - (1.0 / 30)) * 100));
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
+                progress = (double) i / numTerms;
+
+                DecimalFormat dt = new DecimalFormat("#,###");
+                sumLabel.setText("1到" + i + "的阶乘和为： " + df.format(sum));
             }
+        }
+
+        public synchronized void pauseThread() {
+            isThreadPaused = true;
+        }
+
+        public synchronized void resumeThread() {
+            isThreadPaused = false;
+            notify();
         }
     }
 
+    private class ProgressThread extends Thread {
+        public void run() {
+            while (true) {
+                DecimalFormat df = new DecimalFormat("0.00%");
+                progressLabel.setText("Progress: " + df.format(progress));
+                progressBar.setValue((int) (progress * 100));
+
+                try {
+                    Thread.sleep(1000 + rand.nextInt(1000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
